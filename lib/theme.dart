b8b51e45dd938459ed7f33f311adc888d23a5508
lio <shun7018.cod@gmail.com>
2021-11-15 00:40:01 +0900

    change theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -6,10 +6,26 @@ ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
     fontFamily: "muli",
-    appBarTheme: const AppBarTheme(),
+    appBarTheme: appBarTheme(), // 大文字小文字ミス
     textTheme: const TextTheme(),
+    inputDecorationTheme: const InputDecorationTheme(),
     // visual='視覚的' Density='密度' adaptive='順応'
     visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(28),
+    borderSide: const BorderSide(color: kTextColor),
+    gapPadding: 10,
+  );
+  return InputDecorationTheme(
+    floatingLabelBehavior: FloatingLabelBehavior.always,
+    contentPadding: const EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+    enabledBorder: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    border: outlineInputBorder,
   );
 }
 
@@ -23,13 +39,20 @@ TextTheme textTheme() {
 AppBarTheme appBarTheme() {
   return const AppBarTheme(
     titleTextStyle: TextStyle(
-      color: Colors.white,
+      // エラーになるため
+      color: Color(0XFF8B8B8B),
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
     ),
     // color: Colors.white,
-    // elevation: 0,
+    elevation: 0,
     // brightness: Brightness.light,
-    // iconTheme: IconThemeData(color: Colors.black),
-    // textTheme:
-    //     TextTheme(headline6: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18)),
+    backgroundColor: Colors.transparent, // 透明
+    iconTheme: IconThemeData(
+      color: Color(0XFF8B8B8B),
+    ), //color: Colors.black
+    // textTheme: TextTheme(
+    // headline6: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18),
+    // ),
   );
 }
