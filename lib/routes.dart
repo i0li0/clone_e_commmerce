7d04729dfbc350b5212d0bb15ba5a8e9ad54dbaa
lio <shun7018.cod@gmail.com>
2021-11-14 21:20:35 +0900

    add routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,10 @@
+//import 'dart:js';
+
 import 'package:flutter/widgets.dart';
+import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
 import 'package:shop_app/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => const SplashScreen(),
+  SignInScreen.routeName: (context) => const SignInScreen(),
 };
